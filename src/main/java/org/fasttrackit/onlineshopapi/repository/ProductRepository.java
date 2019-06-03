e81809231c747a6c26ccd87d89e5fd92f114eeee
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-06-03 19:00:48 +0300

    studied named queries

--- src/main/java/org/fasttrackit/onlineshopapi/repository/ProductRepository.java
+++ src/main/java/org/fasttrackit/onlineshopapi/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import org.fasttrackit.onlineshopapi.domain.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 // Long is wrapper for primitive type long
 public interface ProductRepository extends JpaRepository<Product, Long> {
@@ -13,5 +14,11 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByNameContainingAndQuantityGreaterThanEqual(
             String partialName, int minQuantity, Pageable pageable);
+
+    // named query using JPQL (Java Persistence Query Language)
+//    @Query("SELECT product FROM Product product WHERE name LIKE '%?1'")
+    // named query using native sql
+    @Query(value = "SELECT * FROM product WHERE name LIKE '%?1'", nativeQuery = true)
+    Page<Product> findByPartialName(String partialName, Pageable pageable);
 
 }
